0f56437a4a1c2019e09fb7a49b693fb6adaa9429
Schlabach <matthewshawn89@gmail.com>
2022-11-11 08:54:58 -0500

    correction

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             var answer = 4;
-            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine”;
-	        Console.Writeline(response);
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+	        Console.WriteLine(response);
         }
     }
 }
